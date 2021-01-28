180eae1752f7a2c5154c3b87c0409b1f1c145a04
sekharb <sekhar.barua@kpit.com>
2021-01-28 16:31:44 +0530

    Sample Producer Consumer Demo

---
+++ ProdCon.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <deque>
+#include <condition_variable>
+
+using namespace std;
+
+std::mutex my_mutex;
+std::condition_variable cond;
+deque<int> buffer;
+const unsigned int buffer_size = 50;
+
+void producer(int val)
+{
+    while(val--)
+    {
+        std::unique_lock<std::mutex>locker(my_mutex);
+        cond.wait(locker,[](){return buffer.size()<buffer_size;});
+        buffer.push_back(val);
+        std::cout << "Producer - produced" << val <<std::endl;
+        locker.unlock();
+        cond.notify_one();
+    }
+}
+
+void consumer()
+{
+    while(true)
+    {
+        std::unique_lock<std::mutex>locker(my_mutex);
+        cond.wait(locker,[](){return buffer.size()>0;});
+        int val = buffer.back();
+        std::cout << "consumer - consumed" << val <<std::endl;
+        locker.unlock();
+        cond.notify_one();
+    }
+}
+
+int main()
+{
+    std::thread t1(producer,100);
+    std::thread t2(consumer);
+
+     t1.join();
+     t2.join();
+}
