7ac53cbda86e41a91ade5e58f1cc17033f0935f8
sekharb <barua.sekhar@gmail.com>
2021-05-23 17:22:24 +0530

    formatting change

--- maxSlidingWindow.cpp
+++ maxSlidingWindow.cpp
@@ -22,11 +22,10 @@ Window position                Max
  1  3  -1  -3  5 [3  6  7]      7
 */
 
-
 #include <iostream>
 #include <vector>
 #include <set>
-#include<math.h>
+#include <math.h>
 #include <algorithm>
 
 using namespace std;
@@ -35,29 +34,32 @@ using namespace std;
 Here use of set is important as the elements are stored in sorted order and we have easy 
 way to find the max element
 */
-vector<int> maxSlidingWindow(vector<int>& nums, int k)
-{  
- std::vector<int> retVec;
+vector<int> maxSlidingWindow(vector<int> &nums, int k)
+{
+    std::vector<int> retVec;
     std::multiset<int> tempSet;
-    if (nums.empty()) return retVec;
-    if (nums.size() == 1) return nums;
-       
-    for(int j=0;j<nums.size();j++)
-    {   
-        
+    if (nums.empty())
+        return retVec;
+    if (nums.size() == 1)
+        return nums;
+
+    for (int j = 0; j < nums.size(); j++)
+    {
         tempSet.emplace(nums.at(j));
-        if (j >= k ) tempSet.erase(tempSet.find(nums.at(j-k)));
-        if (j >= k-1) retVec.emplace_back(*tempSet.rbegin());
-  
+        if (j >= k)
+            tempSet.erase(tempSet.find(nums.at(j - k)));
+        if (j >= k - 1)
+            retVec.emplace_back(*tempSet.rbegin());
     }
     return retVec;
 }
 int main()
 {
-    std::vector<int> inputVec{1,3,-1,-3,5,3,6,7};
+    std::vector<int> inputVec{1, 3, -1, -3, 5, 3, 6, 7};
     int k{3};
-    std::vector<int> retVec = maxSlidingWindow(inputVec,1);
-    for(auto & ele:retVec)
-    std::cout<< ele << "\n" << std::endl;
+    std::vector<int> retVec = maxSlidingWindow(inputVec, 1);
+    for (auto &ele : retVec)
+        std::cout << ele << "\n"
+                  << std::endl;
     return 0;
 }
